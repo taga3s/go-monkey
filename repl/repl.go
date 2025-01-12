0755c9d9cd8b57ca0c00ac0d2dd6fdf10c5d2a5b
taga3s <bepozurapu274@gmail.com>
2025-01-12 23:03:01 +0900

    implement token and lexer

---
+++ repl/repl.go
@@ -0,0 +1,29 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"go-monkey/lexer"
+	"go-monkey/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
