e9dd5be8ff330173b5de152f533bc08b3672467f
taga3s <bepozurapu274@gmail.com>
2025-02-19 21:43:32 +0900

    implement evaluator (integer, boolean, prefixes, and infixes)

--- repl/repl.go
+++ repl/repl.go
@@ -2,6 +2,7 @@ package repl
 
 import (
 	"bufio"
+	"go-monkey/evaluator"
 	"go-monkey/lexer"
 	"go-monkey/parser"
 	"io"
@@ -28,8 +29,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
